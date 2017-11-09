a8260bee5e91208b2f8a030979ed8e7ce058b4d6
Hafidz <halfpheez92@gmail.com>
2017-11-09 15:23:52 +1000

    implemented timers functionality in javascript.

---
+++ timersJS/timers.js
@@ -0,0 +1,8 @@
+// Two methods for timers - setTimeout and SetInterval (single / repeating)
+
+function simpleMessage() {
+	alert("This is just an alert box");
+}
+
+// settimeout is in milliseconds
+setTimeout(simpleMessage,5000);
