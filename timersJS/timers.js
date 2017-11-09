30f31390a48856e0657f5a620f5172b1ab1dbfbd
Hafidz <halfpheez92@gmail.com>
2017-11-09 15:26:49 +1000

    adjusted implementations for timers to change images for every 5 seconds.

--- timersJS/timers.js
+++ timersJS/timers.js
@@ -5,4 +5,21 @@ function simpleMessage() {
 }
 
 // settimeout is in milliseconds
-setTimeout(simpleMessage,5000);
+//setTimeout(simpleMessage,5000);
+
+var myImage = document.getElementById("mainImage");
+
+var imageArray = ["_images/overlook.jpg","_images/winery_sign.jpg","_images/lunch.jpg",
+				  "_images/bigSur.jpg","_images/flag_photo.jpg","_images/mission_look.jpg"];
+var imageIndex = 0;
+
+function changeImage() {
+	myImage.setAttribute("src",imageArray[imageIndex]);
+	imageIndex++;
+	if (imageIndex >= imageArray.length) {
+		imageIndex = 0;
+	}
+}
+
+// setInterval is also in milliseconds
+var intervalHandle = setInterval(changeImage,5000);
