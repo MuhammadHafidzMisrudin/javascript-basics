f5e51b49cd9c5f3dbe3de4459bb03478074e9d88
Hafidz <halfpheez92@gmail.com>
2017-11-10 11:21:45 +1000

    implemented changing message functionality in main message content.

---
+++ changingMessage/changeMessage.js
@@ -0,0 +1,12 @@
+var messageArray = ["The true heart of California","Tours as diverse as California itself","Explore our world your way"];
+var messageIndex = 0;
+
+function deeperFunction() {
+	// perform loop
+	for (var i = 0; i < 500; i++) {
+		// do stuff.
+		var foo = i * (Math.random());
+		var bar = foo;
+		// more exciting code.
+	}
+}
