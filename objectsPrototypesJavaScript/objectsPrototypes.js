377971072398eb2915f65f5a7fd74ff6fcb903cc
Hafidz <halfpheez92@gmail.com>
2017-11-20 18:25:35 +1000

    added a function called logInfo using prototype in JavaScript that returns name string.

--- objectsPrototypesJavaScript/objectsPrototypes.js
+++ objectsPrototypesJavaScript/objectsPrototypes.js
@@ -1,8 +1,14 @@
 // Simple prototype example
 
-function Player(n) {
+function Player(n, s, r) {
 	this.name = n;
+	this.score = s;
+	this.rank = r;
 }
 
+Player.prototype.logInfo = function() {
+	// body...
+	console.log("I am:", this.name);
+}
 
-var fred = new Player("Fred");
+var fred = new Player("Fred", 10000, 5);
