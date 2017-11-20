4f6a2d5e6d62849e4f73e343e886384d8365577f
Hafidz <halfpheez92@gmail.com>
2017-11-20 17:00:27 +1000

    implemented a strict mode implementations in javascript that supported in the browsers.

---
+++ strictModeJavaScript/strictMode.js
@@ -0,0 +1,15 @@
+// didn't use var
+foo = "Hello world";
+
+// new object with two properties with the same name
+var myObject = {
+	samename: 1,
+	samename: 2
+};
+
+// function with the same name for multiple parameters
+function myFunction(a, b, a) {
+	return a + b + c;
+}
+
+document.getElementById("mainContent").innerHTML = foo;
