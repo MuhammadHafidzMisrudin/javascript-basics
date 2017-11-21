4ee53044cbb8ff5857049d5f163343698ce88b4a
Muhammad Hafidz Misrudin <halfpheez92@gmail.com>
2017-11-22 01:58:10 +1000

    added tick() function to handle countdown in seconds and display it on the page. Reset timer when it reaches to zero using clearInterval().

--- countDownExp/countDown.js
+++ countDownExp/countDown.js
@@ -2,6 +2,33 @@
 var secondsRemaining;
 var intervalHandle;
 
+function tick() {
+	// grab the h1
+	var timeDisplay = document.getElementById("time");
+
+	// turn seconds into mm:ss
+	var min = Math.floor(secondsRemaining / 60);
+	var sec = secondsRemaining - (min * 60);
+
+	// add a leading zero (as a string value) if seconds less than 10
+	if (sec < 10) {
+		sec = "0" + sec;
+	}
+	// concatenate with colon
+	var message = min + ":" + sec;
+	// now change the display
+	timeDisplay.innerHTML = message;
+
+	// stop if down to zero
+	if (secondsRemaining === 0) {
+		alert("Done!");
+		clearInterval(intervalHandle);
+		resetPage();
+	}
+	// subtract from seconds remaining
+	secondsRemaining--;
+}
+
 function startCountdown() {
 	// get contents of the "minutes" text box
 	var minutes = document.getElementById("minutes").value;
