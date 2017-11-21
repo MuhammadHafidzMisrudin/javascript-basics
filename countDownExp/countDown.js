04f00e7d833dad7df791781bd1702c86e9eb540d
Muhammad Hafidz Misrudin <halfpheez92@gmail.com>
2017-11-22 01:52:09 +1000

    added startCountdown() to handle an input (minutes) for button. Invoked a button event handler by calling a tick() with setInterval() for every 1 second.

--- countDownExp/countDown.js
+++ countDownExp/countDown.js
@@ -2,6 +2,22 @@
 var secondsRemaining;
 var intervalHandle;
 
+function startCountdown() {
+	// get contents of the "minutes" text box
+	var minutes = document.getElementById("minutes").value;
+	// check if not a number
+	if (isNaN(minutes)) {
+		alert("Please enter a number!");
+		return;
+	}
+	// how many seconds?
+	secondsRemaining = minutes * 60;
+	// every second, call the "tick" function
+	intervalHandle = setInterval(tick, 1000);
+	// hide the form
+	document.getElementById("inputArea").style.display = "none";
+}
+
 // as soon as the page is loaded...
 window.onload = function() {
 	// create input text box and give it an id of "minutes"
