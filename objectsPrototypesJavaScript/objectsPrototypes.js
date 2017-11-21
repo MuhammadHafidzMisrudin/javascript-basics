d6edfed29f985eca055cfc1d779e81a92ca0578b
Muhammad Hafidz Misrudin <halfpheez92@gmail.com>
2017-11-21 22:49:01 +1000

    created a new Player called jane/Jane and invoked same functions.

--- objectsPrototypesJavaScript/objectsPrototypes.js
+++ objectsPrototypesJavaScript/objectsPrototypes.js
@@ -25,7 +25,12 @@ var fred = new Player("Fred", 10000, 5);
 fred.logInfo();
 fred.promote();
 
-// instantiate a new Player
+// instantiate a new Player called bob/Bob
 var bob = new Player("Bob", 5, 1);
 bob.logInfo();
 bob.promote();
+
+// instantiate a new Player called jane/Jane
+var jane = new Player("Jane", 50000, 10);
+jane.logInfo();
+jane.promote();
